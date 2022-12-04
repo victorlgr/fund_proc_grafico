07e35c48562873a98dd96bdb93d7b0ef77f3f456
Victor Luiz Gluz Romano <victorluiz.gromano@gmail.com>
2022-12-04 20:39:08 -0300

    comeco seminario

---
+++ S2/gui.c
@@ -0,0 +1,201 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <math.h>
+#include <string.h>
+
+double massa = 0, mola = 0, amortecedor = 0, F = 0, x0 = 0, v0 = 0;
+int bool_iluminacao = 0, bool_textura = 0;
+GdkRGBA color;
+char array_dados[] = "massa;mola;amortecedor;F;x0;v0;bool_iluminacao;red_iluminacao;green_iluminacao;blue_iluminacao;alpha_iluminacao;bool_textura\n";
+
+GtkWidget *window;
+GtkWidget *fixed;
+GtkWidget *label_massa;
+GtkWidget *input_massa;
+GtkWidget *label_mola;
+GtkWidget *input_mola;
+GtkWidget *label_amortecedor;
+GtkWidget *input_amortecedor;
+GtkWidget *label_F;
+GtkWidget *input_F;
+GtkWidget *label_x0;
+GtkWidget *input_x0;
+GtkWidget *label_v0;
+GtkWidget *input_v0;
+GtkWidget *botao_iluminacao;
+GtkWidget *cor_iluminacao;
+GtkWidget *botao_textura;
+GtkWidget *botao_executar;
+GtkBuilder *builder;
+
+void gerar_csv();
+
+int main(int argc, char *argv[])
+{
+
+    gtk_init(&argc, &argv); // init Gtk
+
+    builder = gtk_builder_new_from_file("gui.glade");
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_builder_connect_signals(builder, NULL);
+
+    fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
+
+    label_massa = GTK_WIDGET(gtk_builder_get_object(builder, "label_massa"));
+    input_massa = GTK_WIDGET(gtk_builder_get_object(builder, "input_massa"));
+
+    label_mola = GTK_WIDGET(gtk_builder_get_object(builder, "label_mola"));
+    input_mola = GTK_WIDGET(gtk_builder_get_object(builder, "input_mola"));
+
+    label_amortecedor = GTK_WIDGET(gtk_builder_get_object(builder, "label_amortecedor"));
+    input_amortecedor = GTK_WIDGET(gtk_builder_get_object(builder, "input_amortecedor"));
+
+    label_F = GTK_WIDGET(gtk_builder_get_object(builder, "label_F"));
+    input_F = GTK_WIDGET(gtk_builder_get_object(builder, "input_F"));
+
+    label_x0 = GTK_WIDGET(gtk_builder_get_object(builder, "label_x0"));
+    input_x0 = GTK_WIDGET(gtk_builder_get_object(builder, "input_x0"));
+
+    label_v0 = GTK_WIDGET(gtk_builder_get_object(builder, "label_v0"));
+    input_v0 = GTK_WIDGET(gtk_builder_get_object(builder, "input_v0"));
+
+    botao_iluminacao = GTK_WIDGET(gtk_builder_get_object(builder, "botao_iluminacao"));
+    cor_iluminacao = GTK_WIDGET(gtk_builder_get_object(builder, "cor_iluminacao"));
+
+    botao_textura = GTK_WIDGET(gtk_builder_get_object(builder, "botao_textura"));
+
+    botao_executar = GTK_WIDGET(gtk_builder_get_object(builder, "botao_executar"));
+
+    gtk_widget_show(window);
+
+    gtk_main();
+
+    return EXIT_SUCCESS;
+}
+
+void on_input_massa_changed(GtkEntry *massa_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(massa_ponteiro));
+    massa = atof(tmp);
+}
+
+void on_input_mola_changed(GtkEntry *mola_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(mola_ponteiro));
+    mola = atof(tmp);
+}
+
+void on_input_amortecedor_changed(GtkEntry *amortecedor_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(amortecedor_ponteiro));
+    amortecedor = atof(tmp);
+}
+
+void on_input_x0_changed(GtkEntry *x0_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(x0_ponteiro));
+    x0 = atof(tmp);
+}
+
+void on_input_forca_changed(GtkEntry *F_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(F_ponteiro));
+    F = atof(tmp);
+}
+
+void on_input_v0_changed(GtkEntry *v0_ponteiro)
+{
+    char tmp[128];
+    sprintf(tmp, "%s", gtk_entry_get_text(v0_ponteiro));
+    v0 = atof(tmp);
+}
+
+void on_botao_executar_clicked(GtkButton *b)
+{
+    gerar_csv();
+}
+
+void on_botao_iluminacao_activate(GtkSwitch *iluminacao_ponteiro)
+{
+    bool_iluminacao = gtk_switch_get_active(iluminacao_ponteiro);
+}
+
+void on_cor_iluminacao_color_set(GtkColorButton *c)
+{
+    gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(c), &color);
+}
+
+void on_botao_textura_activate(GtkSwitch *textura_ponteiro)
+{
+    bool_textura = gtk_switch_get_active(textura_ponteiro);
+}
+
+void comma_to_dot(char *input) {
+    char *ptr = NULL;
+    while(ptr = strpbrk(input, ",")) { //find the first dot in input
+        *ptr = '.'; //replace the dot with a comma
+    }
+}
+
+void gerar_csv()
+{
+    char massa_str[10];
+    char mola_str[10];
+    char amortecedor_str[10];
+    char F_str[10];
+    char x0_str[10];
+    char v0_str[10];
+    char bool_iluminacao_str[10];
+    char bool_textura_str[10];
+    char red_str[10];
+    char green_str[10];
+    char blue_str[10];
+    char alpha_str[10];
+
+    sprintf(massa_str, "%f;", massa);
+    sprintf(mola_str, "%f;", mola);
+    sprintf(amortecedor_str, "%f;", amortecedor);
+    sprintf(F_str, "%f;", F);
+    sprintf(x0_str, "%f;", x0);
+    sprintf(v0_str, "%f;", v0);
+    sprintf(bool_iluminacao_str, "%d;", bool_iluminacao);
+    sprintf(red_str, "%f;", color.red);
+    sprintf(green_str, "%f;", color.green);
+    sprintf(blue_str, "%f;", color.blue);
+    sprintf(alpha_str, "%f;", color.alpha);
+    sprintf(bool_textura_str, "%d", bool_textura);
+
+    strcat(array_dados, massa_str);
+    strcat(array_dados, mola_str);
+    strcat(array_dados, amortecedor_str);
+    strcat(array_dados, F_str);
+    strcat(array_dados, x0_str);
+    strcat(array_dados, v0_str);
+    strcat(array_dados, bool_iluminacao_str);
+    strcat(array_dados, red_str);
+    strcat(array_dados, green_str);
+    strcat(array_dados, blue_str);
+    strcat(array_dados, alpha_str);
+    strcat(array_dados, bool_textura_str);
+
+    comma_to_dot(array_dados);
+
+    FILE *fp;
+    fp = fopen("dados.csv", "w");
+    fputs(array_dados, fp);
+    fclose(fp);
+}
---
+++ S2/mma.c
@@ -0,0 +1,242 @@
+/*
+ * Seminário 1 - Grupo MMA
+ * Cauê Montrose Bianchim, RA 11109715
+ * Ismael Trinca Junior, RA 11131611
+ * Rafael Branco Totino, RA 11201922308
+ * Victor Luiz Gluz Romano, RA 11109915
+ * Compilar o programa com gcc -o mma mma.c -lglut -lGL -lGLU -lm e executar com ./mma X, sendo X a opção desejada (1, 2, 3)
+ *
+ */
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <complex.h>
+#include <sys/stat.h>
+#include <time.h>
+
+double posicao = 0, tempo = 0;
+double massa = 0, mola = 0, amortecedor = 0, F = 0, x0 = 0, v0 = 0;
+int bool_iluminacao = 0, bool_textura = 0;
+double red_iluminacao = 0, green_iluminacao = 0, blue_iluminacao = 0, alpha_iluminacao = 0;
+long int tempo_arquivo = 0, tempo_ultimo_arquivo = 0;
+
+void init(void)
+{
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+   glShadeModel(GL_SMOOTH);
+
+   if(bool_iluminacao){
+      GLfloat light_position[] = {40.0, 20.0, 20.0, 0.0};
+      GLfloat light_ambient[] = {red_iluminacao, green_iluminacao, blue_iluminacao, alpha_iluminacao};
+
+      glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+      glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+      glEnable(GL_LIGHTING);
+      glEnable(GL_LIGHT0);
+      glEnable(GL_DEPTH_TEST);
+   }
+}
+
+double funcao_x_t(double t, double m, double k, double b, double F, double x0, double v0)
+{
+   double potencia = pow(b, 2);
+   double complex raiz = csqrt(potencia - 4 * k * m);
+   double complex C = ((k * raiz - b * k) * x0 - 2 * k * m * v0 - F * raiz + b * F) / (2 * k * raiz);
+   double complex C1 = ((k * raiz + b * k) * x0 + 2 * k * m * v0 - F * raiz - b * F) / (2 * k * raiz);
+   double x = C1 * (cexp(((raiz - b) / 2 * m) * t)) + C * (cexp(((-raiz - b) / 2 * m) * t)) + F / k;
+   return x;
+}
+
+void draw_parede(void)
+{
+   glBegin(GL_LINE_STRIP);
+   glVertex2f(0.0, 80.0);
+   glVertex2f(0.0, 0.0);
+   glVertex2f(100.0, 0.0);
+   glEnd();
+}
+
+void draw_roda(void)
+{
+   GLdouble PI = 3.1415926535897;
+   GLint circle_points = 100;
+   GLfloat angle;
+   glBegin(GL_LINE_LOOP);
+   for (GLint i = 0; i < circle_points; i++)
+   {
+      angle = 2 * PI * i / circle_points;
+      glVertex2f(1.8 * cos(angle), 1.8 * sin(angle));
+   }
+   glEnd();
+}
+
+void draw_massa(void)
+{
+   // Massa
+   glBegin(GL_POLYGON);
+   glVertex2f(0.0, 0.0);
+   glVertex2f(0.0, 25.0);
+   glVertex2f(25.0, 25.0);
+   glVertex2f(25.0, 0.0);
+   glEnd();
+
+   // Roda
+   glTranslatef(5, -2, 0);
+   draw_roda();
+   glTranslatef(15, 0, 0);
+   draw_roda();
+}
+
+void draw_mola(int comprimento, double x)
+{
+   int y = 3;
+   glBegin(GL_LINE_STRIP);
+   glVertex2f(0.0, 0.0);
+   for (int i = 1; i < comprimento; i++)
+   {
+      if (i % 2 == 0)
+         y = -3;
+      else
+         y = 3;
+      glVertex2f(x * i, y);
+   }
+   glVertex2f(x * comprimento, 0);
+   glEnd();
+}
+
+void draw_amortecedor(double x)
+{
+   // Suporte
+   glRotatef(90, 0, 0, 1);
+   glBegin(GL_LINE_STRIP);
+   glVertex2f(0, 10);
+   glVertex2f(0, -5);
+   glVertex2f(-4, -5);
+   glVertex2f(-4, -25);
+   glVertex2f(-3, -25);
+   glVertex2f(-3, -6);
+   glVertex2f(4, -6);
+   glVertex2f(4, -25);
+   glVertex2f(5, -25);
+   glVertex2f(5, -5);
+   glVertex2f(1, -5);
+   glVertex2f(1, 10);
+   glEnd();
+
+   // Pistão
+   glTranslatef(1, -19, 0);
+   glTranslatef(0, x, 0);
+   glRotatef(-180, 0, 0, 1);
+   glBegin(GL_LINE_STRIP);
+   glVertex2f(0, 11);
+   glVertex2f(0, -5);
+   glVertex2f(-3, -5);
+   glVertex2f(-3, -6);
+   glVertex2f(4, -6);
+   glVertex2f(4, -5);
+   glVertex2f(1, -5);
+   glVertex2f(1, 11);
+   glEnd();
+}
+
+void display(void)
+{
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   // glColor3f(1.0, 1.0, 1.0);
+
+   glLoadIdentity();
+   glTranslatef(-40, -40, 0);
+   draw_parede();
+
+   glLoadIdentity();
+   glTranslatef(posicao, -36, 0);
+   draw_massa();
+
+   glLoadIdentity();
+   glTranslatef(-40, -15, 0);
+   draw_mola(20, 2.0 + posicao / 20);
+
+   glLoadIdentity();
+   glTranslatef(-30, -30, 0);
+   draw_amortecedor(-posicao);
+
+   glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   if (w <= h)
+      glOrtho(-50.0, 50.0, -50.0 * (GLfloat)h / (GLfloat)w,
+              50.0 * (GLfloat)h / (GLfloat)w, -50.0, 50.0);
+   else
+      glOrtho(-50.0 * (GLfloat)w / (GLfloat)h,
+              50.0 * (GLfloat)w / (GLfloat)h, -50.0, 50.0, -50.0, 50.0);
+   glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key)
+   {
+   case 27:
+      exit(0);
+      break;
+   }
+}
+
+long int checa_arquivo()
+{
+   struct stat attr;
+   stat("dados.csv", &attr);
+   return attr.st_mtime;
+}
+
+void ler_arquivo()
+{
+   FILE *fp;
+   fp = fopen("dados.csv", "r");
+   fscanf(fp, "%*[^\n]\n%lf;%lf;%lf;%lf;%lf;%lf;%d;%lf;%lf;%lf;%lf;%d", &massa, &mola, &amortecedor, &F, &x0, &v0, &bool_iluminacao, &red_iluminacao, &green_iluminacao, &blue_iluminacao, &alpha_iluminacao, &bool_textura);
+   fclose(fp);
+}
+
+void timer(int placeholder)
+{
+   glutPostRedisplay();
+   glutTimerFunc(1000 / 60, timer, 1);
+   tempo_ultimo_arquivo = checa_arquivo();
+   if (tempo_ultimo_arquivo > tempo_arquivo)
+   {
+      tempo_arquivo = checa_arquivo();
+      tempo = 0;
+      posicao = 0;
+      ler_arquivo();
+   }
+   tempo++;
+   double x_t = funcao_x_t(tempo, massa, mola, amortecedor, F, x0, v0);
+   posicao = x_t;
+}
+
+int main(int argc, char **argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize(500, 500);
+   glutInitWindowPosition(100, 100);
+   glutCreateWindow("mma.c");
+   init();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutKeyboardFunc(keyboard);
+   glutTimerFunc(1000, timer, 0);
+   glutMainLoop();
+   return 0;
+}
